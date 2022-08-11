167b75089114ce433ef1d3cf61319f45927ba824
Anubhav Bhardwaj <61362008+abd8126@users.noreply.github.com>
2022-08-11 11:50:18 +0530

    Spring Boot with war file reation - mvn spring-boot:run - mvn build - mvn install

---
+++ src/main/java/TomcatController.java
@@ -0,0 +1,20 @@
+import java.util.Collection;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+public class TomcatController {
+
+    @GetMapping("/hello")
+    public Collection<String> sayHello() {
+        return IntStream.range(0, 10)
+                .mapToObj(i -> "Hello number " + i)
+                .collect(Collectors.toList());
+    }
+    public static void main(String[] args) {
+        SpringApplication.run(TomcatController.class, args);
+    }
+}
